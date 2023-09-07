b5c6ed075e663a269507757d2faf82d63d141714
zohida <zohishka04@gmail.com>
2023-09-07 14:14:03 +0500

    "Site finished"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var elHeader = document.querySelector('.site-header');
-var elBtn = document.querySelector('.menu');
+var elBtn = document.querySelector('.site-header__btn');
 
-elBtn.addEventListener('.click',() => {
-    elHeader.classList.toggle('open');
+elBtn.addEventListener('.click', () => {
+    elHeader.classList.toggle('.open');
 });
